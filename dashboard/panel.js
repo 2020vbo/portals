ba646968a83b2034c5aea2b1d529d561ed52eb6d
Victor Bo <victorbo2020@gmail.com>
2024-03-10 04:59:34 -0400

    tesseract works with obs fullscreen preview. numbers hardcoded

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -1,11 +1,62 @@
 // You can access the NodeCG api anytime from the `window.nodecg` object
 // Or just `nodecg` for short. Like this!:
+
 document.querySelector("#set").addEventListener("click", setPortalName);
+const portalReplicant = nodecg.Replicant("portalName");
 
 function setPortalName() {
     const portal = document.querySelector("#setPortal").value;
-    const portalReplicant = nodecg.Replicant("portalName");
 
     console.log("??");
     portalReplicant.value = portal;
+}
+
+portalReplicant.on("change", value => {
+    document.querySelector("#setPortal").value = value;
+});
+
+const Tesseract = window.Tesseract;
+
+const worker = await Tesseract.createWorker("eng");
+let intervalID = null;
+
+const options = {
+    video: {
+        displaySurface: "window"
+    },
+    audio: false
+};
+
+const video = document.querySelector("#screenshare");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+async function startCapture(displayMediaOptions) {
+    try {
+        video.srcObject = 
+            await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        console.log(video.srcObject);
+        intervalID = setInterval(mirror, 1000);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+startCapture(options);
+
+function mirror() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx.globalCompositeOperate = "source-over";
+    ctx.filter = "grayscale(100%)";
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);    
+
+    (async () => {
+        const { data: { text} } = await worker.recognize(canvas, {
+            rectangle: { top: 515, left: 628, width: 664, height: 88 },
+        });
+        console.log(text);
+        portalReplicant.value = text.replace(/[^\w\s]/g, "");
+        // await worker.terminate();
+    })();
 }
--- graphics/index.js
+++ graphics/index.js
@@ -1,61 +1,60 @@
-// You can access the NodeCG api anytime from the `window.nodecg` object
-// Or just `nodecg` for short. Like this!:
-import Tesseract from "https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.esm.min.js";
+// don't need cuz of cdn
+// import Tesseract from "https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.esm.min.js";
+
+// const Tesseract = window.Tesseract;
 
 const portalReplicant = nodecg.Replicant("portalName");
 portalReplicant.on("change", value => {
     document.querySelector("#portal").innerHTML = value;
 });
 
-// const { createWorker } = require("tesseract.js");
+// const worker = await Tesseract.createWorker("eng");
 
-const worker = await Tesseract.createWorker("eng");
-// const image = new File(["img"], "testing/augpay.png");
+// Tesseract on img element test
+// const img = document.getElementById("testimg");
 
-const img = document.getElementById("testimg");
+// (async () => {
+//     const { data: { text} } = await worker.recognize(img);
+//     console.log(text);
+//     portalReplicant.value = text;
+//     // await worker.terminate();
+// })();
 
-// console.log("a");
-// console.log(image.lastModifiedDate);
-// console.log("b");
+// const options = {
+//     video: {
+//         displaySurface: "window"
+//     },
+//     audio: false
+// };
 
-(async () => {
-    const { data: { text} } = await worker.recognize(img);
-    console.log(text);
-    portalReplicant.value = text;
-    // await worker.terminate();
-})();
+// const video = document.querySelector("#screenshare");
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+// ctx.filter = "grayscale(100%)";
 
-const options = {
-    video: {
-        displaySurface: "window"
-    },
-    audio: false
-};
+// async function startCapture(displayMediaOptions) {
+//     try {
+//         video.srcObject = 
+//             await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+//         console.log(video.srcObject);
+//         setInterval(mirror, 1000);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
 
-const video = document.querySelector("#screenshare");
-const canvas = document.getElementById("canvas");
+// startCapture(options);
 
-async function startCapture(displayMediaOptions) {
-    try {
-        video.srcObject = 
-            await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-        console.log(video.srcObject);
-        setInterval(mirror, 1000);
-    } catch (err) {
-        console.error(err);
-    }
-}
+// function mirror() {
+//     canvas.width = video.videoWidth;
+//     canvas.height = video.videoHeight;
+//     ctx.globalCompositeOperate = "source-over";
+//     ctx.filter = "grayscale(100%)";
+//     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);    
 
-startCapture(options);
-
-function mirror() {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
-
-    (async () => {
-        const { data: { text} } = await worker.recognize(canvas);
-        console.log(text);
-        // await worker.terminate();
-    })();
-}
+//     (async () => {
+//         const { data: { text} } = await worker.recognize(canvas);
+//         console.log(text);
+//         // await worker.terminate();
+//     })();
+// }
