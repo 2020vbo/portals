2fd81d73c9ef2da446f86271fa86157f13788a84
Victor Bo <victorbo2020@gmail.com>
2024-09-16 15:40:03 -0400

    created game start, read portal. need to actually create states

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -59,15 +59,39 @@ function mirror() {
     canvas.height = video.videoHeight;
     ctx.globalCompositeOperate = "source-over";
     ctx.filter = "grayscale(100%)";
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);    
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    (async () => {
-        const { data: { text} } = await worker.recognize(canvas, {
-            rectangle: { top: 515, left: 628, width: 664, height: 88 },
-        });
-        console.log(text);
-        portalReplicant.value = text.replace(/[^\d\w\s-\']/g, "");
-        // await worker.terminate();
-    })();
+    readPortal();
 }
 
+async function detectGameStart() {
+    const { data: { hp1 } } = await worker.recognize(canvas, {
+        rectangle: { top: 206, left: 1825, width: 25, height: 15 },
+    });
+
+    const { data: { hp2 } } = await worker.recognize(canvas, {
+        rectangle: { top: 206, left: 1825, width: 25, height: 15 },
+    });
+
+    const { data: { hp3 } } = await worker.recognize(canvas, {
+        rectangle: { top: 206, left: 1825, width: 25, height: 15 },
+    });
+    console.log([hp1, hp2, hp3]);
+
+    // trigger game started state if 2/3 are = 100, listen for portal
+}
+
+async function readPortal() {
+    const { data: { text } } = await worker.recognize(canvas, {
+        rectangle: { top: 515, left: 628, width: 664, height: 88 },
+    });
+    console.log(text);
+    portalReplicant.value = text.replace(/[^\d\w\s-\']/g, "");
+    // await worker.terminate();
+
+    // trigger portal selected state, listen for end of game
+}
+
+async function detectGameEnd() {
+    // check for nth place text, trigger end of game, listen for next game
+}
